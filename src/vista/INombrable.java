30d10139d05b9718d01f1628a6685d25ef9ec6d6
gebonilla <blukers@hotmail.com>
2015-03-09 14:10:10 -0500

    Clases e interfaces iniciales de la vista

---
+++ src/vista/INombrable.java
@@ -0,0 +1,7 @@
+package vista;
+
+public interface INombrable 
+{
+	public Object getNombre();
+	public void setNombre(Object nombre);
+}
---
+++ src/vista/Vista.java
@@ -0,0 +1,34 @@
+package vista;
+
+import java.awt.List;
+
+public abstract class Vista 
+{
+	private List controles;
+	private INombrable nombre;
+	
+	public void AgregarControl(Object control)
+	{
+		
+	}
+	
+	public Object GetControl(int index)
+	{
+		return null;
+	}
+	
+	public Object EliminarControl(int index)
+	{
+		return null;
+	}
+	
+	public void abrir()
+	{
+		
+	}
+	public void cerrar()
+	{
+		
+	}
+	
+}
